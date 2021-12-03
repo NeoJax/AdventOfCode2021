36b95baa39df9f75d8e137bf09e0ebaed1627da7
Jack Lubaway <jlubaway@uwm.com>
2021-12-03 17:06:40 -0500

    Day 3 Complete!

--- AdventOfCode2021/AdventOfCode2021/Program.cs
+++ AdventOfCode2021/AdventOfCode2021/Program.cs
@@ -10,19 +10,25 @@ namespace AdventOfCode2021
         static void Main(string[] args)
         {
             Console.WriteLine("Day 1 \nPuzzle 1");
-            List<int> day1Test1 = ChangeListToInt(ReadText("Day1Test1"));
-            List<int> day1Puzzle1 = ChangeListToInt(ReadText("Day1Puzzle1"));
-            Console.WriteLine(Day1.Puzzle1(day1Test1));
-            Console.WriteLine(Day1.Puzzle1(day1Puzzle1));
+            List<int> day1Test = ChangeListToInt(ReadText("Day1Test"));
+            List<int> day1Puzzle = ChangeListToInt(ReadText("Day1Puzzle"));
+            Console.WriteLine(Day1.Puzzle1(day1Test));
+            Console.WriteLine(Day1.Puzzle1(day1Puzzle));
             Console.WriteLine("\nPuzzle 2");
-            Console.WriteLine(Day1.Puzzle2(day1Test1));
-            Console.WriteLine(Day1.Puzzle2(day1Puzzle1));
+            Console.WriteLine(Day1.Puzzle2(day1Test));
+            Console.WriteLine(Day1.Puzzle2(day1Puzzle));
             Console.WriteLine("\nDay 2 \nPuzzle 3");
-            Console.WriteLine(Day2.CalculateFinalPosition(ReadText("Day2Test1")));
-            Console.WriteLine(Day2.CalculateFinalPosition(ReadText("Day2Puzzle1")));
+            Console.WriteLine(Day2.CalculateFinalPosition(ReadText("Day2Test")));
+            Console.WriteLine(Day2.CalculateFinalPosition(ReadText("Day2Puzzle")));
             Console.WriteLine("\nPuzzle 4");
-            Console.WriteLine(Day2.CalculateActualFinalPosition(ReadText("Day2Test1")));
-            Console.WriteLine(Day2.CalculateActualFinalPosition(ReadText("Day2Puzzle1")));
+            Console.WriteLine(Day2.CalculateActualFinalPosition(ReadText("Day2Test")));
+            Console.WriteLine(Day2.CalculateActualFinalPosition(ReadText("Day2Puzzle")));
+            Console.WriteLine("\nDay 3 \nPuzzle 5");
+            Console.WriteLine(Day3.DiagnosticReport(ReadText("Day3Test")));
+            Console.WriteLine(Day3.DiagnosticReport(ReadText("Day3Puzzle")));
+            Console.WriteLine("\nPuzzle 6");
+            Console.WriteLine(Day3.TrueDiagnosticReport(ReadText("Day3Test")));
+            Console.WriteLine(Day3.TrueDiagnosticReport(ReadText("Day3Puzzle")));
         }
 
         public static List<string> ReadText(string puzzlePath)
---
+++ AdventOfCode2021/AdventOfCode2021/PuzzleCode/Day3.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.PuzzleCode
+{
+    public class Day3
+    {
+        public static double DiagnosticReport(List<string> report)
+        {
+            List<int> commonBitsZero = new List<int>();
+            List<int> commonBitsOne = new List<int>();
+            int lineLength = report[0].Length;
+            StringBuilder gammaBinary = new StringBuilder();
+            StringBuilder epsilonBinary = new StringBuilder();
+            double gammaRate = 0;
+            double epsilonRate = 0;
+
+            for (int i = 0; i < lineLength; i++)
+            {
+                commonBitsZero.Add(0);
+                commonBitsOne.Add(0);
+            }
+
+            foreach (string line in report)
+            {
+                for (int i = 0; i < lineLength; i++)
+                {
+                    switch (line[i])
+                    {
+                        case '0':
+                            commonBitsZero[i]++;
+                            break;
+                        case '1':
+                            commonBitsOne[i]++;
+                            break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < lineLength; i++)
+            {
+                if (commonBitsZero[i] > commonBitsOne[i])
+                {
+                    gammaBinary.Append("0");
+                    epsilonBinary.Append("1");
+                }
+                else
+                {
+                    gammaBinary.Append("1");
+                    epsilonBinary.Append("0");
+                }
+            }
+
+            gammaRate = ConvertToDouble(gammaBinary.ToString());
+            epsilonRate = ConvertToDouble(epsilonBinary.ToString());
+
+            return gammaRate * epsilonRate;
+        }
+
+        public static double TrueDiagnosticReport(List<string> report)
+        {
+            List<string> oxygenList = report;
+            List<string> scrubberList = report;
+            double oxygenRating = 0;
+            double scrubberRating = 0;
+            int lineLength = report[0].Length;
+
+            for (int i = 0; i < lineLength; i++)
+            {
+                if (oxygenList.Count == 2)
+                {
+                    oxygenList = oxygenList.Where(str => str[lineLength - 1] == '1').ToList();
+                    break;
+                }
+                if (oxygenList.Count == 1)
+                {
+                    break;
+                }
+
+                oxygenList = oxygenList.Where(str => str[i] == MostCommonBit(oxygenList, i, true)).ToList();
+            }
+
+            for (int i = 0; i < lineLength; i++)
+            {
+                if (scrubberList.Count == 2)
+                {
+                    scrubberList = scrubberList.Where(str => str[lineLength - 1] == '0').ToList();
+                    break;
+                }
+                if (scrubberList.Count == 1)
+                {
+                    break;
+                }
+
+                scrubberList = scrubberList.Where(str => str[i] == MostCommonBit(scrubberList, i, false)).ToList();
+            }
+
+            oxygenRating = ConvertToDouble(oxygenList[0]);
+            scrubberRating = ConvertToDouble(scrubberList[0]);
+
+            return oxygenRating * scrubberRating;
+        }
+
+
+        private static double ConvertToDouble(string binary)
+        {
+            double doubleBinary = 0;
+            char[] reverseString = binary.ToCharArray();
+            Array.Reverse(reverseString);
+            string reverseBinary = new string(reverseString);
+
+            for (int i = 0; i < reverseBinary.Length; i++)
+            {
+                if (reverseBinary[i] == '1')
+                {
+                    doubleBinary += Math.Pow(2, i);
+                }
+            }
+
+            return doubleBinary;
+        }
+
+        private static char MostCommonBit(List<string> binaryLines, int index, bool oxygen)
+        {
+            char bestBit;
+            int commonBitsZero = 0;
+            int commonBitsOne = 0;
+
+            foreach (string line in binaryLines)
+            {
+                switch (line[index])
+                {
+                    case '0':
+                        commonBitsZero++;
+                        break;
+                    case '1':
+                        commonBitsOne++;
+                        break;
+                }
+            }
+
+            if (commonBitsOne == commonBitsZero)
+            {
+                return oxygen ? '1' : '0';
+            }
+
+            bestBit = commonBitsOne > commonBitsZero ? '1' : '0';
+
+            if (!oxygen)
+            {
+                return bestBit == '0' ? '1' : '0';
+            }
+
+            return bestBit;
+        }
+    }
+}
