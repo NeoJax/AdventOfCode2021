a1da9a2aa95227f36bba1fcae14e479abb87a752
Jack Lubaway <jlubaway@uwm.com>
2021-12-03 10:55:51 -0500

    Finished Day 1!

--- AdventOfCode2021/AdventOfCode2021/Program.cs
+++ AdventOfCode2021/AdventOfCode2021/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2021.PuzzleCode;
 
 namespace AdventOfCode2021
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> day1Test1 = ChangeListToInt(ReadText("Day1Test1"));
+            List<int> day1Puzzle1 = ChangeListToInt(ReadText("Day1Puzzle1"));
+            Console.WriteLine(Day1.Puzzle1(day1Test1));
+            Console.WriteLine(Day1.Puzzle1(day1Puzzle1));
+            Console.WriteLine(Day1.Puzzle2(day1Test1));
+            Console.WriteLine(Day1.Puzzle2(day1Puzzle1));
+        }
+
+        public static List<string> ReadText(string puzzlePath)
+        {
+            List<string> input = System.IO.File.ReadAllLines(@"..\\..\\..\\PuzzleInput\\" + puzzlePath + ".txt").ToList();
+
+            return input;
+        }
+
+        public static List<int> ChangeListToInt(List<string> stringList)
+        {
+            List<int> parsedList = new List<int>();
+
+            foreach (string s in stringList)
+            {
+                parsedList.Add(Int32.Parse(s));
+            }
+
+            return parsedList;
         }
     }
 }
---
+++ AdventOfCode2021/AdventOfCode2021/PuzzleCode/Day1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.PuzzleCode
+{
+    public static class Day1
+    {
+        public static int Puzzle1(List<int> depths)
+        {
+            int prevDepth = Int32.MaxValue;
+            int increaseCount = 0;
+            for (int i = 0; i < depths.Count; i++)
+            {
+                int currentDepth = depths[i];
+                if (currentDepth > prevDepth)
+                {
+                    increaseCount++;
+                }
+
+                prevDepth = currentDepth;
+            }
+            return increaseCount;
+        }
+
+        public static int Puzzle2(List<int> depths)
+        {
+            int prevThreeDepths = Int32.MaxValue;
+            int increaseCount = 0;
+
+            for (int i = 0; i < depths.Count-2; i++)
+            {
+                int currentThreeDepths = depths[i] + depths[i + 1] + depths[i + 2];
+                if (currentThreeDepths > prevThreeDepths)
+                {
+                    increaseCount++;
+                }
+
+                prevThreeDepths = currentThreeDepths;
+            }
+
+            return increaseCount;
+        }
+    }
+}
